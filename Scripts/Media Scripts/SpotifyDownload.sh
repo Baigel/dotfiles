f2c74c039725e0cbfec28c4964a4fc0a7d9be97a
Ahmed <ahmed@entersoft.com.au>
2020-12-30 21:11:37 +1000

    fixed script to be more new-user friendly

--- Scripts/Media Scripts/SpotifyDownload.sh
+++ Scripts/Media Scripts/SpotifyDownload.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-echo "Using Master Account Playlists"
-cd
-cd ~/OneDrive/Music/Baigels_Tunes
-rm baigels_tunes.txt
-spotdl -f ~/OneDrive/Music/Baigels_Tunes -p https://open.spotify.com/playlist/4rru4z2JcnQXI8CpbFT00j?si=XnKh-IYiSk20vPK_udz8RQ # Get playlist and pass into baigels_tunes.txt
+
+# PLAYLIST_URL must be set (can get url by right clicking spotify playlist -> Share -> Copy Playlist Link
+# Should also replace playlist_name.txt with a meaningful name (or not; its up to you)
+
+PLAYLIST_URL=""
+
+cd ~/Music
+spotdl -f ~/Music/playlist_name -p PLAYLIST_URL # Get playlist and pass into playlist_name.txt
 touch downloadedSongs.txt # In case it does not exist
-diff -e downloadedSongs.txt baigels_tunes.txt | grep "https" > newSongLinks.txt # Get new songs
+diff -e downloadedSongs.txt playlist_name.txt | grep "https" > newSongLinks.txt # Get new songs
 cat newSongLinks.txt >> downloadedSongs.txt # Keep a record of all songs in downloadedSongs.txt
-spotdl -f ~/OneDrive/Music/Baigels_Tunes --overwrite skip -l newSongLinks.txt # Download songs
+spotdl -f ~/Music/playlist_name.txt --overwrite skip -l newSongLinks.txt # Download songs
 rm newSongLinks.txt
 cd
-onedrive --synchronize
