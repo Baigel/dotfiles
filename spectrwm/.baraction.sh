532ec7d6c8490e7b263ea7781a14bf9125195378
Ahmed <ahmed@entersoft.com.au>
2021-02-15 22:22:57 +1000

    working on baraction, fixed zsh bindings in .shellrc

--- spectrwm/.baraction.sh
+++ spectrwm/.baraction.sh
@@ -1,101 +1,63 @@
-# #################### BAIGEL's SPECTRWM CUSTOM BARACTION ####################
- 
-# Bash commands that need to be run on startup
-feh --bg-scale ~/.wallpaper
-xmodmap .xmodmaprc
- 
- print_date() {
-     # The date is printed to the status bar by default.
-     # To print the date through this script, set clock_enabled to 0
-     # in spectrwm.conf.  Uncomment "print_date" below.
-     FORMAT="%a %b %d %R %Z %Y"
-     DATE=`date "+${FORMAT}"`
-     echo -n "${DATE}     "
- }
- 
- print_mem() {
-     MEM=`/usr/bin/top | grep Free: | cut -d " " -f6`
-     echo -n "Free mem: $MEM  "
- }
- 
- _print_cpu() {
-     printf "CPU: %3d%% User %3d%% Nice %3d%% Sys %3d%% Spin %3d%% Int %3d%% Idle  " $1 $2 $3 $4 $5 $6
- }
- 
- print_cpu() {
-     OUT=""
-     # iostat prints each column justified to 3 chars, so if one counter
-     # is 100, it jams up agains the preceeding one. sort this out.
-     while [ "${1}x" != "x" ]; do
-         if [ ${1} -gt 99 ]; then
-             OUT="$OUT ${1%100} 100"
-             else
-                 OUT="$OUT ${1}"
-                 fi
-                 shift;
-             done
-             _print_cpu $OUT
- }
- 
- print_cpuspeed() {
-     CPU_SPEED=`/sbin/sysctl hw.cpuspeed | cut -d "=" -f2`
-     printf "CPU speed: %4d MHz  " $CPU_SPEED
- }
- 
- print_bat() {
-     BAT_STATUS=$1
-     BAT_LEVEL=$2
-     AC_STATUS=$3
-     
-     if [ $AC_STATUS -ne 255 -o $BAT_STATUS -lt 4 ]; then
-         if [ $AC_STATUS -eq 0 ]; then
-             echo -n "on battery (${BAT_LEVEL}%)"
-             else
-                 case $AC_STATUS in
-                 1)
-                 AC_STRING="on AC: "
-                 ;;
-             2)
-             AC_STRING="on backup AC: "
-             ;;
-         *)
-         AC_STRING=""
-         ;;
-         esac;
-         case $BAT_STATUS in
-         4)
-         BAT_STRING="(no battery)"
-         ;;
-         [0-3])
-         BAT_STRING="(battery ${BAT_LEVEL}%)"
-         ;;
-         *)
-         BAT_STRING="(battery unknown)"
-         ;;
-         esac;
-         
-         FULL="${AC_STRING}${BAT_STRING}"
-         if [ "$FULL" != "" ]; then
-             echo -n "$FULL"
-             fi
-             fi
-             fi
- }
- 
- # cache the output of apm(8), no need to call that every second.
- APM_DATA=""
- I=0
- while :; do
-     IOSTAT_DATA=`/usr/sbin/iostat -C -c 2 | tail -n 1 | grep '[0-9]$'`
-     if [ $I -eq 0 ]; then
-         APM_DATA=`/usr/sbin/apm -alb`
-         fi
-         # print_date
-         print_mem
-         print_cpu $IOSTAT_DATA
-         print_cpuspeed
-         print_bat $APM_DATA
-         echo ""
-         I=$(( ( ${I} + 1 ) % 11 ))
-         sleep 1
-done
+#!/bin/bash
+# baraction.sh for spectrwm status bar
+# From http://wiki.archlinux.org/index.php/Scrotwm
+
+SLEEP_SEC=5
+#loops forever outputting a line every SLEEP_SEC secs
+while :; do
+    # power output
+    BAT=`acpi -b`
+    CHARGE=`acpi -b | awk '{printf("%d", $4)}'`
+    AC=`acpi -a | awk '{printf("%s", $3)}'`
+    # if AC connected
+    if [ "$AC" = 'on-line' ]
+        then
+        #if battery connected and charging
+        # TO DO check
+        if [ -n "$BAT" ]
+            then
+            # TO DO make % green to show charged/ing
+            POWER="${CHARGE}% ch"
+            else                                                                                                                                         
+                #is only on AC                                                                                                                       
+                POWER="AC"                                                                                                                           
+                fi
+                else # hence is not on AC so is discharging battery
+                    POWER="${CHARGE}% disch"
+                    # if [ "$CHARGE -gt "20" ]
+                    # then
+                    # #TO DO make orange for > 20%
+                    # POWER="${CHARGE}%"
+                    # else
+                    # #TO DO make red for < 20%# POWER="${CHARGE}%"
+                    # fi
+                    fi
+                    POWER_STR="Bat:$POWER"
+                    
+                    #spectrwm bar_print can't handle UTF-8 characters, such as degree symbol
+                    #Core 0:      +67.0?C  (crit = +100.0?C)
+                    eval $(sensors 2>/dev/null | sed s/[?+]//g | awk '/^Core 0/ {printf "CORE0TEMP=%s;", $3}; /^Core 1/ {printf "CORE1TEMP=%s;",$3}; /^fan1/ {printf "FANSPD=%s;",$2};' -)
+                    TEMP_STR="Tcpu=$CORE0TEMP,$CORE1TEMP"
+                    
+                    WLAN_ESSID=$(iwconfig wlp3s0 | awk -F "\"" '/wlp3s0/ { print $2 }')
+                    #eval $(cat /proc/net/wireless | sed s/[.]//g | awk '/wlp3s0/ {printf "WLAN_QULTY=%s; WLAN_SIGNL=%s; WLAN_NOISE=%s", $3,$4,$5};' -)
+                    #BCSCRIPT="scale=0;a=100*$WLAN_QULTY/70;print a"
+                    #WLAN_QPCT=`echo $BCSCRIPT | bc -l`
+                    #WLAN_POWER=`iwconfig 2>/dev/null| grep "Tx-Power"| awk {'print $4'}|sed s/Tx-Power=//`
+                    WLAN_STR="$WLAN_ESSID"
+                    
+                    #CPUFREQ_STR=`echo "Freq:"$(cat /proc/cpuinfo | grep 'cpu MHz' | sed 's/.*: //g; s/\..*//g;')`
+                    CPULOAD_STR="Load:$(uptime | sed 's/.*://; s/,//g')"
+                    
+                    eval $(awk '/^MemTotal/ {printf "MTOT=%s;", $2}; /^MemFree/ {printf "MFREE=%s;",$2}' /proc/meminfo)
+                    MUSED=$(( $MTOT - $MFREE ))
+                    MUSEDPT=$(( ($MUSED * 100) / $MTOT ))
+                    MEM_STR="Mem:${MUSEDPT}%"
+                    
+                    echo -e "$POWER_STR  $TEMP_STR  $CPULOAD_STR  $MEM_STR  $WLAN_STR"
+                    #alternatively if you prefer a different date format
+                    #DATE_STR=`date +"%H:%M %a %d %b`
+                    #echo -e "$DATE_STR   $POWER_STR  $TEMP_STR  $CPUFREQ_STR  $CPULOAD_STR  $MEM_STR  $WLAN_STR"
+                    
+                    sleep $SLEEP_SEC
+                    done
