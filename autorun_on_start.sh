7a5d6b3928b6472d244e585ba5710fc1e3028d0d
Baigel <ahafeezbaig@gmail.com>
2021-07-03 08:53:19 +1000

    spectrwm and shell preferences update

--- autorun_on_start.sh
+++ autorun_on_start.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
 # Key Mappings
-xmodmap -e "keycode 133=Alt_L"                                                                                                 
-xmodmap -e "keycode 64=Super_L"                                                                                                
-
-# Bash Aliases
-alias nano="nano -l"
+xmodmap -e "keycode 133=Alt_L"
+xmodmap -e "keycode 64=Super_L"
--- spectrwm/.baraction.sh
+++ spectrwm/.baraction.sh
@@ -18,9 +18,9 @@ while :; do
             then
             # TO DO make % green to show charged/ing
             POWER="+${CHARGE}%"
-        else                                                                                                                                         
-            #is only on AC                                                                                                                       
-            POWER="AC"                                                                                                                           
+        else
+            #is only on AC
+            POWER="AC"
         fi
     else # hence is not on AC so is discharging battery
         POWER="-${CHARGE}%"
@@ -33,29 +33,29 @@ while :; do
         # fi
     fi
     POWER_STR="Bat: $POWER"
-    
+
     #spectrwm bar_print can't handle UTF-8 characters, such as degree symbol
     eval $(sensors 2>/dev/null | sed s/[?+]//g | awk '/^Core 0/ {printf "CORE0TEMP=%s;", $3}; /^Core 1/ {printf "CORE1TEMP=%s;",$3}; /^Core 2/ {printf "CORE2TEMP=%s;",$3}; /^Core 3/ {printf "CORE3TEMP=%s;",$3}; /^fan1/ {printf "FANSPD=%s;",$2};' -)
     TEMP_STR="CPU Temp: $CORE0TEMP, $CORE1TEMP, $CORE2TEMP, $CORE3TEMP"
-    
+
     NETWORK=`nmcli | grep ' connected' | cut -d " " -f 4`
     [ -z "$NETWORK" ] && NETWORK="Disconnected"
     NET_STR="Net: $NETWORK"
-    
+
     CPUFREQ_STR=`echo "CPU (GHz): "$(cat /proc/cpuinfo | grep 'cpu MHz' | sed 's/.*: //g; s/\..*//g;' | awk '{ total += $1; count++ } END { print total/count }' | awk '{print int($1)/1000}')`
-    
+
     CPULOAD_STR="Load: $(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')"
-	
+
     eval $(awk '/^MemTotal/ {printf "MTOT=%s;", $2}; /^MemFree/ {printf "MFREE=%s;",$2}' /proc/meminfo)
     MUSED=$(( $MTOT - $MFREE ))
     MUSEDPT=$(( ($MUSED * 100) / $MTOT ))
     MEM_STR="Mem: ${MUSEDPT}%"
-    
+
     DATE_STR=$(date | cut -d " " -f 1-5)
-	
+
 	VOLUME="Vol: $(awk -F"[][]" '/Left:/ { print $2 }' <(amixer sget Master))"
 
-    echo -e "$DATE_STR    |    $POWER_STR    |    $TEMP_STR    |    $CPUFREQ_STR    |    $CPULOAD_STR    |    $MEM_STR    |    $VOLUME    |    $NET_STR"
-    
+    echo -e "$NET_STR    |    $POWER_STR    |    $TEMP_STR    |    $CPUFREQ_STR    |    $CPULOAD_STR    |    $MEM_STR    |    $VOLUME    |    $DATE_STR"
+
     sleep $SLEEP_SEC
 done
