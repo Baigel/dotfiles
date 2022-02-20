55c9ae3c268e4e7c912b395cdd0e5aec13f6ccfb
Baigel <ahafeezbaig@gmail.com>
2022-02-21 04:30:53 +1000

    moved scripts to another repo

--- Scripts/scrcpyPhone.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-adb tcpip 5555
-adb connect 192.168.8.3:5555
-sleep 1
-scrcpy --bit-rate 2M --max-size 800
--- autorun_on_start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Key Mappings
-xmodmap -e "keycode 133=Alt_L"
-xmodmap -e "keycode 64=Super_L"
